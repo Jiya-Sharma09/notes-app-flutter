5b43654d5aa94048dbc9e268ed3d8b3739574f4f
Jiya-Sharma09 <jiyas4870@gmail.com>
2026-07-15 19:54:08 +0530

    add(auth provider): auth provider with login function

---
+++ lib/provider/auth-provider.dart
@@ -0,0 +1,36 @@
+import 'package:provider/provider.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:notes_app_flutter/services/api_client.dart';
+import 'package:notes_app_flutter/services/auth_service.dart';
+import 'package:flutter/foundation.dart';
+
+class AuthProvider extends ChangeNotifier {
+  String baseURL = "";
+  final storage = FlutterSecureStorage();
+  late final ApiClient _client = ApiClient(baseUrl:baseURL);
+  late final AuthService  _authService = AuthService(_client);
+  String? _token;
+
+  AuthProvider();
+
+  String? get token => _token;
+  
+  Future<void> login({required String email, required String password})async
+  {
+    
+    try {
+       _token = await _authService.login(email: email, password: password);
+    } catch (e) {
+      throw Exception('Login failed: $e');
+      
+    }
+    
+    try{
+      await storage.write(key: "token", value: _token);
+    }catch(e){
+      throw Exception('Failed to store token: $e');
+    }
+    
+    notifyListeners();
+  }
+}
