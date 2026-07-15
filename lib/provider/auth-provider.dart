bb31696171eb26febf9e0f579b36017643351806
Jiya-Sharma09 <jiyas4870@gmail.com>
2026-07-15 23:18:34 +0530

    add(auth-provider): loading state for cleanre UI

--- lib/provider/auth-provider.dart
+++ lib/provider/auth-provider.dart
@@ -9,40 +9,54 @@ class AuthProvider extends ChangeNotifier {
   late final ApiClient _client = ApiClient(baseUrl: baseURL);
   late final AuthService _authService = AuthService(_client);
   String? _token;
+  bool isLoading = false;
 
   String? get token => _token;
 
   Future<void> login({required String email, required String password}) async {
+    isLoading = true;
+    notifyListeners();
     _token = await _authService.login(email: email, password: password);
 
     try {
       await storage.write(key: "token", value: _token);
     } catch (e) {
       _token = null;
+      isLoading = false;
       notifyListeners();
       throw Exception('Failed to store token: $e');
     }
+    isLoading = false;
     notifyListeners();
   }
 
   Future<void> logout() async {
+    isLoading = true;
+    notifyListeners();
     try {
       await storage.delete(key: "token");
     } catch (e) {
-      throw Exception('Failed to logout!');
+      isLoading = false;
+      notifyListeners();
+      throw Exception('Failed to logout: $e');
     }
+    isLoading = false;
     _token = null;
     notifyListeners();
   }
 
-  Future<void> autoLogin() async{
-    try{
+  Future<void> autoLogin() async {
+    isLoading = true;
+    notifyListeners();
+    try {
       _token = await storage.read(key: "token");
+      isLoading = false;
       notifyListeners();
-    }catch(e){
+    } catch (e) {
       _token = null;
+      isLoading = false;
       notifyListeners();
-      throw Exception('Failed to auto-login!');
+      throw Exception('Failed to login: $e');
     }
   }
 }
