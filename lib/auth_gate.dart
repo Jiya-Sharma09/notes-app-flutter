c61508730b8ec3dc79f326ea8d3b0d57ed9fb89d
Jiya-Sharma09 <jiyas4870@gmail.com>
2026-07-22 22:04:42 +0530

    feat(auth-gate): final minimal version.

--- lib/auth_gate.dart
+++ lib/auth_gate.dart
@@ -4,18 +4,26 @@ import 'package:notes_app_flutter/provider/auth-provider.dart';
 import 'package:notes_app_flutter/screens/login_screen.dart';
 import 'package:notes_app_flutter/screens/home_screen.dart';
 
-class AuthGate extends StatelessWidget {
-  final Widget child;
+class AuthGate extends StatefulWidget {
+  const AuthGate({super.key});
+  @override
+  State<AuthGate> createState() => _AuthGateState();
+}
 
-  const AuthGate({super.key, required this.child});
+class _AuthGateState extends State<AuthGate> {
+  @override
+  void initState() {
+    super.initState();
+    context.read<AuthProvider>().autoLogin();
+  }
 
   @override
   Widget build(BuildContext context) {
-    final authProvider = context.read<AuthProvider>();
-
-    authProvider.autoLogin();
-    final token = authProvider.token;
-    if (token == null) {
+    final authProvider = context.watch<AuthProvider>();
+    if (authProvider.isLoading) {
+      // TODO: add navigation to splash screen!
+    }
+    if (authProvider.token == null) {
       return const LoginScreen();
     } else {
       return const HomeScreen();
