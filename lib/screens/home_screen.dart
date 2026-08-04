2e5527292388281b801963ff95163cf78234d2e5
Jiya-Sharma09 <jiyas4870@gmail.com>
2026-08-04 11:59:29 +0530

    feat: implement notes display in HomeScreen and add NoteTile widget

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,22 +1,56 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:notes_app_flutter/provider/auth-provider.dart';
+import 'package:animated_search_bar/animated_search_bar.dart';
+import 'package:notes_app_flutter/provider/notes-provider.dart';
+import 'package:notes_app_flutter/widget/note_tile.dart';
+import 'package:notes_app_flutter/services/auth_service.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final notesProvider = context.watch<NotesProvider>();
+    final notes = notesProvider.notes;
+    final _authService = context.read<AuthProvider>();
+
     return Scaffold(
-      appBar: AppBar(title: const Text('Home')),
-      body: Center(
-        child: IconButton(
-          icon: const Icon(Icons.logout),
-          onPressed: () async {
-            await context.read<AuthProvider>().logout();
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () {
+            _authService.logout();
           },
+          icon: Icon(Icons.logout),
         ),
       ),
+      body: SingleChildScrollView(
+  child: Padding(
+    padding: const EdgeInsets.fromLTRB(20, 12, 20, 20),
+    child: Column(
+      children: [
+        // Hero section
+        // Search bar
+
+        notes.isEmpty
+            ? const Center(child: Text("No notes yet"))
+            : ListView.separated(
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                itemCount: notes.length,
+                separatorBuilder: (_, __) => const SizedBox(height: 10),
+                itemBuilder: (context, index) {
+                  final note = notes[index];
+                  return NoteTile(
+                    note: note,
+                    onTap: () {},
+                  );
+                },
+              ),
+      ],
+    ),
+  ),
+),
     );
   }
 }
